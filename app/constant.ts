6ed39fa79eac66683282f66211fa722e80fa1464
niaoren7015 <33828503+niaoren7015@users.noreply.github.com>
2025-04-12 11:58:52 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -824,7 +824,7 @@ export const internalAllowedWebDavEndpoints = [
   "https://app.koofr.net/dav/Koofr",
 ];
 
-export const DEFAULT_GA_ID = "G-89WN60ZK2E";
+//export const DEFAULT_GA_ID = "G-89WN60ZK2E";
 
 export const SAAS_CHAT_URL = "https://gpt.100000.best";
 export const SAAS_CHAT_UTM_URL = "https://gpt.100000.best";
